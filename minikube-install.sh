8fe7c6319da7389a51f517afcd834edb6a808ad7
kannancloud001 <129275611+kannancloud001@users.noreply.github.com>
2024-12-08 20:49:13 +0530

    Delete minikube-install.sh

--- minikube-install.sh
+++
@@ -1,9 +0,0 @@
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-chmod +x kubectl
-mkdir -p ~/.local/bin
-mv ./kubectl ~/.local/bin/kubectl
-minikube start --driver=docker --force
